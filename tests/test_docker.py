afac146c0103b6aa05ce6ba49d97a733208d78fa
Paweł Krupa <pkrupa@sointeractive.pl>
2017-06-02 15:35:30 +0200

    fix fix tests

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -4,7 +4,7 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
 def test_letsencrypt_folder(host):
-#    letsencrypt = host.file("/etc/letsencrypt")
+    # letsencrypt = host.file("/etc/letsencrypt")
     letsencrypt = host.file("/var/log/certbot")
     assert letsencrypt.is_directory
     assert letsencrypt.user == "root"
