16630bf031c96541c496ad73dc8724e8db3744a8
Paweł Krupa <pkrupa@sointeractive.pl>
2017-06-02 15:31:16 +0200

    fix tests

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -2,6 +2,7 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
+
 def test_letsencrypt_folder(host):
 #    letsencrypt = host.file("/etc/letsencrypt")
     letsencrypt = host.file("/var/log/certbot")
